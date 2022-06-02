918159e7eccd120215e1f8ac15a9e18e900566e5
Danny Rodriguez <danny007111@hotmail.com>
2022-06-02 00:25:39 -0500

    Making changes

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+function Links(prop){
+    return (
+        <>
+            <h3>Links</h3>
+            <a href="prop.links.github"></a>
+            <a href="prop.links.linkedin"></a>
+        </>
+    );
+};
+
+export default Links;
